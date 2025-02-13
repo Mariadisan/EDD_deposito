be086ef743bec899624e6f505cfacfbf42554789
unknown <mariadisan@outlook.com>
2025-02-13 13:24:18 +0100

    refactorizando proyecto deposito

---
+++ src/cuenta/Main.java
@@ -0,0 +1,28 @@
+package cuenta;
+public class Main {
+
+    private static CCuenta cuenta1;
+
+	public static void main(String[] args) {
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        double saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es" + saldoActual);
+        
+
+        operativa_cuenta(-2300);
+        operativa_cuenta(695);
+    }
+	private static void operativa_cuenta(float cantidad) {
+		try {
+			if (cantidad > 0) {
+	            System.out.println("Ingreso en cuenta");
+	            cuenta1.ingresar(cantidad);
+			} else {
+				System.out.println("Retirada de cuenta");
+				cuenta1.retirar(-cantidad);
+			} 
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar o ingresar");
+        }
+	}
+}
