bbdd6fe0032f95aa2a06f5ccc16f606fc1b90c62
unknown <mariadisan@outlook.com>
2025-02-15 14:01:26 +0100

    eliminado el archivo Main2.java

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cuenta","l":"CCuenta"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cuenta","l":"CCuenta"},{"p":"cuenta","l":"Main"}];updateSearchResults();
